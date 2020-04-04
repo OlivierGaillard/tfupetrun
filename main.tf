7699925a9d86b7cb2ad3de286ee589b637f0bd00
Olivier Gaillard <info@metacoherence.ch>
2020-04-04 17:28:20 +0200

    Basic web server

--- main.tf
+++ main.tf
@@ -4,11 +4,39 @@ provider "aws" {
   profile                 = "tfupetrun"
 }
 
+variable "server_port" {
+  description = "The port number used for HTTP access"
+  type        = number
+  default     = 8080
+}
 resource "aws_instance" "example" {
-    ami           = "ami-0ec1ba09723e5bfac"
-    instance_type = "t2.micro"
+    ami                    = "ami-0b418580298265d5c"
+    instance_type          = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
+
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World" > index.html
+                nohup busybox httpd -f -p ${var.server_port} &
+                EOF
 
     tags = {
       Name = "terraform-example"
     }
+
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "public_ip" {
+  value   = aws_instance.example.public_ip
+  description = "The public IP address of the web server"
 }
